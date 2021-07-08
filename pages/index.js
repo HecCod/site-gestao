e22cc7670f46382a0e7fd4989901148dee58073d
HecCod <heitorcallipo14@gmail.com>
2021-07-07 21:18:55 -0300

    pagina antiga

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,7 @@ function Home() {
         <div>
             <h1>Home</h1>
             <p>This is a prototype site.</p>
-            <style>
-                h1 {
-                    border-style: ridge;
-                    border-color: black;
-                    border-width: 5px;
-                }
-            </style>
+            
             <Contador />
         </div>
     )
